1e9b23ad58c3b07cfee0464bb776d53882cde2f6
Matt Snyder <snyder2112@me.com>
2013-06-14 09:09:28 -0400

    Implement Flat theme

---
+++ vendor/assets/javascripts/flat.js
@@ -0,0 +1,28 @@
+//= require flat/plugins/jquery-ui/jquery.ui.core.min
+//= require flat/plugins/jquery-ui/jquery.ui.widget.min
+//= require flat/plugins/jquery-ui/jquery.ui.mouse.min
+//= require flat/plugins/jquery-ui/jquery.ui.draggable.min
+//= require flat/plugins/jquery-ui/jquery.ui.resizable.min
+//= require flat/plugins/jquery-ui/jquery.ui.sortable.min
+//= require flat/plugins/touch-punch/jquery.touch-punch.min
+//= require flat/plugins/slimscroll/jquery.slimscroll.min
+//= require flat/bootstrap
+//= require flat/plugins/vmap/jquery.vmap.min
+//= require flat/plugins/vmap/jquery.vmap.world
+//= require flat/plugins/vmap/jquery.vmap.sampledata
+//= require flat/plugins/bootbox/jquery.bootbox
+//= require flat/plugins/flot/jquery.flot.min
+//= require flat/plugins/flot/jquery.flot.bar.order.min
+//= require flat/plugins/flot/jquery.flot.pie.min
+//= require flat/plugins/flot/jquery.flot.resize.min
+//= require flat/plugins/imagesLoaded/jquery.imagesloaded.min
+//= require flat/plugins/pageguide/jquery.pageguide
+//= require flat/plugins/fullcalendar/fullcalendar.min
+//= require flat/plugins/chosen/chosen.jquery.min
+//= require flat/plugins/select2/select2.min
+//= require flat/plugins/icheck/jquery.icheck.min
+//= require flat/eakroko
+//= require flat/application
+
+
+
